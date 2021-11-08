9cb29cbba9a6d8c66dcfea4de67296e6940f3edf
Lindomar-silva <lindomar.s.silva>
2021-11-08 17:19:31 -0300

    Alterando BASE_URL para 'http://177.../dsvendas';

--- frontend/src/utils/request.ts
+++ frontend/src/utils/request.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
+// export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://177.103.164.233/dsvendas';
